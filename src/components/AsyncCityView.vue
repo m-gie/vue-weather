7665963874340a76fabe163a264d9b386a3e3de6
m.glowacki <m.glowacki@smsnet.pl>
2024-04-24 15:00:29 +0200

    Add finished forecast page

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -12,11 +12,11 @@
       <p class="text-sm">Data last updated at: {{ weatherForecastResults.current.last_updated }}</p>
       <p class="text-8xl mt-12">{{ weatherForecastResults.current.temp_c }}°C</p>
       <p class="text-sm">Feels like: {{ weatherForecastResults.current.feelslike_c }}°C</p>
-      <div class="mt-6 flex flex-wrap items-center">
+      <div class="mt-6 flex flex-wrap items-center h-32">
         <img
           :src="`${weatherForecastResults.current.condition.icon}`"
           alt="weather icon"
-          class="w-128 h-128"
+          width="128"
         />
         {{ weatherForecastResults.current.condition.text }}
       </div>
@@ -24,18 +24,66 @@
     <hr class="border border-white w-full border-opacity-25" />
     <div v-if="weatherForecastResults !== null" class="max-w-screen-md w-full py-12">
       <div class="mx-8 text-white">
-        <h2 class="mb-4">Hourly weather</h2>
-        <div class="flex gap-5 overflow-x-scroll">
+        <h2 class="mb-4">Today, hourly weather</h2>
+        <div class="flex gap-5 overflow-x-auto">
           <div
             v-bind:key="hourData.time_epoch"
-            v-for="hourData in weatherForecastResults.forecast.forecastday[0].hour"
-            class="flex flex-col gap-4 items-center"
+            v-for="hourData in todayHours"
+            class="flex flex-col gap-2 items-center"
           >
             <p>{{ hourData.time.split(' ')[1] }}</p>
             <div class="w-[64px] h-[64px]">
               <img :src="`${hourData.condition.icon}`" alt="weather icon" />
             </div>
-            <p class="text-xs">{{ hourData.chance_of_rain }}%</p>
+            <p class="text-xs">
+              <i class="fa-solid fa-cloud-rain"></i> {{ hourData.chance_of_rain }}%
+            </p>
+            <p class="text-xs">{{ hourData.temp_c }}°C</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="border border-white w-full border-opacity-25" />
+    <div v-if="weatherForecastResults !== null" class="max-w-screen-md w-full pt-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Three day forecast</h2>
+        <div class="flex gap-5 overflow-x-auto">
+          <div
+            v-bind:key="dayData.date_epoch"
+            v-for="dayData in weatherForecastResults.forecast.forecastday"
+            @click="setForecastDayData(dayData)"
+          >
+            <div
+              :class="`flex flex-col gap-2 items-center ${forecastDayData?.date === dayData.date ? 'bg-weather-secondary rounded-lg' : ''}`"
+            >
+              <p>{{ dayData.date }}</p>
+              <div class="w-[96px] h-[96px] flex items-center justify-center">
+                <img :src="`${dayData.day.condition.icon}`" alt="weather icon" width="96" />
+              </div>
+              <p><i class="fa-solid fa-cloud-rain"></i> {{ dayData.day.daily_chance_of_rain }}%</p>
+              <p>{{ dayData.day.avgtemp_c }}°C</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="hourForecastDay" v-if="forecastDayData !== null" class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">{{ forecastDayData.date }}, hourly weather</h2>
+        <div class="flex gap-5 overflow-x-auto">
+          <div
+            v-bind:key="hourData.time_epoch"
+            v-for="hourData in forecastDayData.hour"
+            class="flex flex-col gap-2 items-center"
+          >
+            <p>{{ hourData.time.split(' ')[1] }}</p>
+            <div class="w-[64px] h-[64px]">
+              <img :src="`${hourData.condition.icon}`" alt="weather icon" />
+            </div>
+            <p class="text-xs">
+              <i class="fa-solid fa-cloud-rain"></i> {{ hourData.chance_of_rain }}%
+            </p>
+            <p class="text-xs">{{ hourData.temp_c }}°C</p>
           </div>
         </div>
       </div>
@@ -45,50 +93,8 @@
 
 <script setup lang="ts">
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import { useRoute } from 'vue-router'
-
-interface CurrentWeatherProps {
-  location: {
-    name: string
-    region: string
-    country: string
-    lat: number
-    lon: number
-    tz_id: string
-    localtime_epoch: number
-    localtime: string
-  }
-  current: {
-    last_updated_epoch: number
-    last_updated: string
-    temp_c: number
-    temp_f: number
-    is_day: number
-    condition: {
-      text: string
-      icon: string
-      code: number
-    }
-    wind_mph: number
-    wind_kph: number
-    wind_degree: number
-    wind_dir: string
-    pressure_mb: number
-    pressure_in: number
-    precip_mm: number
-    precip_in: number
-    humidity: number
-    cloud: number
-    feelslike_c: number
-    feelslike_f: number
-    vis_km: number
-    vis_miles: number
-    uv: number
-    gust_mph: number
-    gust_kph: number
-  }
-}
 
 interface ForecastHourProps {
   time_epoch: number
@@ -220,33 +226,31 @@ interface WeatherForecastProps {
 
 const route = useRoute()
 
-const weatherApiResults = ref<CurrentWeatherProps | null>(null)
 const weatherForecastResults = ref<WeatherForecastProps | null>(null)
+const todayHours = ref<ForecastHourProps[] | null>(null)
+const forecastDayData = ref<ForecastDayProps | null>(null)
 
-const getWeatherData = async () => {
-  try {
-    const weatherData = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHERAPI_KEY}&q=${route.params.city}`
-    )
-    weatherApiResults.value = weatherData.data
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
+const setForecastDayData = async (dayData: ForecastDayProps) => {
+  forecastDayData.value = dayData
+  await nextTick()
+  const hourForecastDiv = document.getElementById('hourForecastDay')
+  hourForecastDiv!.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
 }
 
 const getWeatherForecast = async () => {
   try {
     const weatherForecast = await axios.get(
-      `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHERAPI_KEY}&q=${route.params.city}&qdays=3`
+      `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHERAPI_KEY}&q=${route.params.city}&days=3`
     )
     weatherForecastResults.value = weatherForecast.data
+    todayHours.value = weatherForecastResults.value!.forecast.forecastday[0].hour.filter(
+      (hourObject) => hourObject.time_epoch * 1000 > Date.now() - 3600000
+    )
   } catch (error) {
     console.log(error)
     throw error
   }
 }
 
-const weatherForecast = await getWeatherForecast()
-// const weatherData = await getWeatherData()
+await getWeatherForecast()
 </script>
